a83de71777ffea95063df5acb9be7a687ea77d9f
Ali <aliaghayev132@gmail.com>
2024-08-21 02:25:06 +0400

    Delete Added

--- src/pages/Shops/ShopsPage.jsx
+++ src/pages/Shops/ShopsPage.jsx
@@ -14,6 +14,17 @@ import {
 // console.log(`public/uploads/${shop._id}/shop.photo`)
 // console.log(`public/uploads/${shop._id}/shop.logo`)
 
+const deleteShop = async (id) =>{
+  try{
+    const res = await fetch(`${import.meta.env.VITE_API_GLOBAL_URL}/api/shop/delete?id=${id}`,{
+      method:"DELETE",
+    });
+
+  }catch(error){
+    console.error(error);
+  }
+}
+
 const fetchShops = async () => {
   const res = await fetch(`${import.meta.env.VITE_API_GLOBAL_URL}/api/shop`);
   if (!res.ok) {
@@ -46,6 +57,8 @@ const AddShopModal = ({ setShowAddModal }) => {
       [name]: file,
     });
   };
+
+  
   
   const mutation = useMutation(
     async (formData) => {
@@ -181,8 +194,22 @@ const AddShopModal = ({ setShowAddModal }) => {
   );
 };
 
+
 function ShopsPage() {
+  const queryClient = useQueryClient();
   const [showAddModal, setShowAddModal] = useState(false);
+  const deleteMutation = useMutation({
+    mutationFn: deleteShop,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['shops']);
+      console.log('Shop deleted successfully');
+    },
+    onError: (error) => {
+      // Hata oluştuğunda yapılacak işlemler
+      console.error('Error deleting shop:', error.message);
+    }
+  });
+  
 
   const { isLoading, isError, isSuccess, data, error } = useQuery(
     "shops",
@@ -261,7 +288,7 @@ function ShopsPage() {
                 <button className="px-3 py-2 bg-green-600 text-white rounded-md">
                   <Pencil size={18} />
                 </button>
-                <button className="px-3 ms-2 py-2 bg-red-600 text-white rounded-md">
+                <button onClick={() => deleteMutation.mutate(shop._id)} className="px-3 ms-2 py-2 bg-red-600 text-white rounded-md">
                   <Trash2 size={18} />
                 </button>
               </td>
